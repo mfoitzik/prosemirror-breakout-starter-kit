28d7e85e9fc4674bd00154b42edfd19f2356f5a7
mfoitzik <mike@mifo.com>
2020-03-02 06:02:39 -0800

    Cleaned up files

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-// code{
     import {EditorState, TextSelection} from "prosemirror-state"
     import {EditorView} from "prosemirror-view"
     import {Schema, DOMParser, DOMSerializer} from "prosemirror-model"
@@ -6,6 +5,8 @@
     import {addListNodes} from "prosemirror-schema-list"
     import {exampleSetup} from "./customSetup"
     import '../css/prose.css'
+    import '../css/mystyles.scss'
+    var pretty = require('pretty')
     // Mix the nodes from prosemirror-schema-list into the basic schema to
     // create a schema with list support.
     const mySchema = new Schema({
@@ -19,7 +20,7 @@
         plugins: exampleSetup({schema: mySchema})
       })
     })
-    // }
+
   
     let myt = document.querySelector("#mytext");
     document.getElementById("mybutton").addEventListener("click", function(){
@@ -30,7 +31,7 @@
         .serializeFragment(view.state.doc.content)
   
       div.appendChild(fragment)
-      myt.value = div.innerHTML;
+      myt.value = pretty(div.innerHTML);
       div.remove();
     }); 
   
@@ -38,23 +39,12 @@
       let newcontent = myt.value;
       const newdiv = document.createElement('div');
       newdiv.innerHTML = newcontent;
-      //console.log(view.dispatch);
-      //below line "works"
-      //view.dispatch(state.tr.insertText("kpkpk"));
-      //let mysel = new state.TextSelection(1, 2);
-      //view.dispatch(state.tr.replaceSelectionWith(DOMParser.fromSchema(schema).parse(newdiv)));
-      //view.state.doc.content = DOMParser.fromSchema(schema).parse(newdiv);
-  
-  
-      //from tip
       const { doc, tr } = view.state;
       console.log(view.state.doc.content.size);
-      //const document = this.createDocument(content, parseOptions)
       const selection = TextSelection.create(doc, 0, doc.content.size);
       const transaction = tr
         .setSelection(selection)
         .replaceSelectionWith(DOMParser.fromSchema(mySchema).parse(newdiv, { preserveWhiteSpace: true }), false);
-      //let mkmk = view.dispatch(transaction);
       let mkmk = view.state.apply(transaction);
       view.updateState(mkmk);
       console.log(view.state.doc.content.size);
--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,6 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
-    // Path and filename of your result bundle.
-    // Webpack will bundle all JavaScript into this file
     plugins: [
       
         new MiniCssExtractPlugin({
@@ -46,30 +44,20 @@ module.exports = {
           }
         },
         {
-            // Apply rule for .sass, .scss or .css files
             test: /\.(sa|sc|c)ss$/,
 
-            // Set loaders to transform files.
-            // Loaders are applying from right to left(!)
-            // The first loader will be applied after others
             use: 
             [
                 {
-                    // After all CSS loaders we use plugin to do his work.
-                    // It gets all transformed CSS and extracts it into separate
-                    // single bundled file
                     loader: MiniCssExtractPlugin.loader
                 },
                 {
-                    // This loader resolves url() and @imports inside CSS
                     loader: "css-loader",
                 },
                 {
-                    // Then we apply postCSS fixes like autoprefixer and minifying
                     loader: "postcss-loader"
                 },
                 {
-                    // First we transform SASS to standard CSS
                     loader: "sass-loader",
                     options: {
                         implementation: require("sass")
